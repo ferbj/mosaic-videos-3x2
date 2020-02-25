fa2d177fda3dacd27f991bedcb3f7630d987c4ef
fernando balta <ferbj200@gmail.com>
2020-02-24 22:22:58 -0500

    first commit

---
+++ index.js
@@ -0,0 +1,120 @@
+const fs = require('fs');
+const path = require('path');
+const ffmpegInstaller = require('@ffmpeg-installer/ffmpeg');
+const ffmpeg = require('fluent-ffmpeg');
+
+ffmpeg.setFfmpegPath(ffmpegInstaller.path);
+
+let command = ffmpeg()
+
+// Change this to the desired output resolution  
+
+let x=960, y=720;
+
+let videoInfo = [];
+
+let savepath = 'D:/mosaic-videos/uploadvideos';
+const folderpath = path.join(savepath);
+fs.readdir(folderpath, (err, files) => {
+    if(!files || files.length === 0) {
+        console.log(`provided folder '${folderpath}' is empty or does not exist.`);
+        console.log('Make sure your project was compiled!');
+        return;
+      }
+    
+      files.forEach(function(file , idx) {
+        
+          const filePath = path.join(folderpath,file);
+           // ignore if directory
+             if (fs.lstatSync(filePath).isDirectory()) {
+                next;
+             }
+             fs.readFile(filePath, (error, fileContent) => {
+                // if unable to read file contents, throw exception
+                if (error) { throw error; }
+                    let filename = file;
+                    console.log(idx + ': Input File ... ' + filename);
+                    videoInfo.push({			
+                        filename: filePath
+                    });
+    
+                    command = command.addInput(filePath);
+      
+              });
+
+              cords(x,y,idx,files);              
+      });
+    process(x,y,videoInfo);
+
+});
+
+
+function cords(x,y,idx){
+            switch(idx){
+                case 0:
+                    videoInfo[0] = { coord : { x: 0, y: y/6 } }
+                break;
+                case 1:
+                     videoInfo[1] = { coord : { x:  x/3, y: y/6} }
+                break;
+                case 2: 
+                     videoInfo[2] = { coord : { x: x/1.5, y: y/6 }}
+                break;
+                case 3: 
+                     videoInfo[3] = { coord: { x: 0, y: y/2 }}
+                break;
+                case 4: 
+                    videoInfo[4] = { coord: { x: x/3, y: y/2 }}
+                break;
+                case 5:
+                    videoInfo[5] =  { coord: { x: x /1.5, y: y/2 }}
+                break;
+                    
+            }
+    return videoInfo;
+}
+ 
+function process(x,y,index,array){
+    console.log('videoInfo', videoInfo)
+    var complexFilter = [];
+    complexFilter.push('nullsrc=size=' + x + 'x' + y + ' [base0]');
+
+// Scale each video
+videoInfo.forEach(function (val, index, array) {
+    complexFilter.push({
+        filter: 'setpts=PTS-STARTPTS, scale', options: [x/3, y/3],
+        inputs: index+':v', outputs: 'block'+index
+    });
+    
+});
+
+// Build Mosaic, block by block
+videoInfo.forEach(function (val, index, array) {
+   complexFilter.push({
+        filter: 'overlay', options: {  x: val.coord.x, y: val.coord.y },
+        inputs: ['base'+index, 'block'+index], outputs: 'base'+(index+1)
+    });
+});
+console.log('complexFilter',complexFilter)
+var outFile = 'Fileout.mp4';
+//duration of video and building of mosaic video
+
+command
+    .duration(45)
+    .videoCodec('libx264')
+    .audioCodec('libmp3lame')
+    .format('mp4')
+    .complexFilter(complexFilter, 'base6')
+    .save(outFile)
+    .on('error', function(err) {
+        console.log('An error occurred: ' + err.message);
+    })	
+    .on('progress', function(progress) { 
+        console.log('... frames: ' + progress.frames);
+    })
+    .on('end', function() { 
+        console.log('Finished processing'); 
+    });
+
+}
+
